4159151679f3cabc744a317d8e79addbcaebb4d8
Dev-ModScript <dieulinh16092012@gmail.com>
2026-07-20 06:11:29 +0700

    Create Kaitun Real.lua

---
+++ Kaitun Real.lua
@@ -0,0 +1,22 @@
+Config = {
+    -- "Pirates" or "Marines"
+    Team = "Marines",
+    Configuration = {
+        HopWhenIdle = true,
+        AutoHop = true,
+        AutoHopDelay = 3,
+        FpsBoost = true,
+        blackscreen = true,          
+    },
+    Items = {
+        AutoFullyMelees = true,
+        Saber = true,
+        CursedDualKatana = false,
+        SoulGuitar = true,
+        RaceV2 = true,
+    },
+    Settings = {
+        StayInSea2UntilHaveDarkFragments = true,
+    }
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-ModScript/Var-Hub/refs/heads/main/Var%20Hub%20Kaitun.lua"))()
